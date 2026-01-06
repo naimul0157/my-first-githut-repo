94f36246deb2a16614e6499764f8a108a6a5d7de
naimul0157 <naimul794@gmail.com>
2026-01-07 01:18:15 +0600

    first commit

---
+++ bit.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    // int n;
+    // cin>>n;
+    int x=0;
+    // for (int i = 0; i < n; i++)
+    // {
+        string s="fab++";
+        // cin>>s;
+        cout<<s.find("y");
+        // if (s.find("++"))
+        // {
+        //     x++;
+        // }
+        // else{
+        //     x--;
+        // }
+        
+    // }
+    // cout<<x<<endl;
+    
+    return 0;
+}
